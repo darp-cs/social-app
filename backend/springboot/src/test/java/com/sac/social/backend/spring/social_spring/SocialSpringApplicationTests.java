e10a0f2db2e6a2dae26477055e66f5f2f7c4b0f6
CameronTwiggs <camtwiggs@proton.me>
2025-04-05 05:10:24 -0400

    intial maven/spring setup

---
+++ backend/springboot/src/test/java/com/sac/social/backend/spring/social_spring/SocialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sac.social.backend.spring.social_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
