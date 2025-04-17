d3fead68b59c52791d8d90ee2562c2e6252b5811
darp-cs <diegoarod14@gmail.com>
2025-04-16 23:51:36 -0400

    Initial setup with exxample code that needs refactoring

--- backend/diesel/src/lib.rs
+++ backend/diesel/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub mod models;
+pub mod schema;
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+
+use self::models::{NewPost, Post};
+
+pub fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    MysqlConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn create_post(conn: &mut MysqlConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let new_post = NewPost { title, body };
+
+    conn.transaction(|conn| {
+        diesel::insert_into(posts::table)
+            .values(&new_post)
+            .execute(conn)?;
+
+        posts::table
+            .order(posts::id.desc())
+            .select(Post::as_select())
+            .first(conn)
+    })
+    .expect("Error while saving post")
 }
