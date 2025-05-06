da8b8f58a69a0a0b0df819938afffaa6cc8af759
darp-cs <diegoarod14@gmail.com>
2025-05-05 21:07:47 -0400

    Added encryption to password and added login endpoint, but it will change to just return auth and might move from this route to another

--- backend/fastapi/app/core/security/utils.py
+++ backend/fastapi/app/core/security/utils.py
@@ -1,10 +1,21 @@
-from database.models.user import User, UserCreateValidation
+from AesEverywhere import aes256
+from database.models.user import UserCreateValidation, User
 
-
-# TODO: Implement hashing function
 def hash_password(password: str) -> str:
-    return password
+    hashed_password = aes256.encrypt(password,'key')
+    return hashed_password
 
+
+def verify_password(hashed_password: str, password: str) -> bool:
+    try:
+        decrypt_password  = aes256.decrypt(hashed_password,'key').decode('utf-8')
+        if password != decrypt_password:
+            return False
+        return True
+    except Exception as e:
+        print(f"Error decrypting password: {e}")
+        return False
+    
 def prepare_user(user:UserCreateValidation) -> dict:
     prepared_user = User.model_validate(user,update={"password_hash": hash_password(user.password)})
     return prepared_user
